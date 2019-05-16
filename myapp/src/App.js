116466876c68682656c0ee6eb3fad815d58bad1d
Ben Dibuz <ben.dibuz@gmail.com>
2019-05-16 06:09:35 -0500

    continue

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Ninjas from './Ninjas'
+import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
   state = {
@@ -9,11 +10,19 @@ class App extends Component {
       {name: 'John', age: '15', belt: 'green', id: 3},
     ]
   }
+  addNinja = (ninja) => {
+      ninja.id = Math.random();
+      let ninjas = [...this.state.ninjas, ninja];
+      this.setState({
+        ninjas: ninjas
+      })
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React app</h1>
         <Ninjas ninjas = {this.state.ninjas}/>
+        <AddNinja addNinja = {this.addNinja}/>
       </div>
     );
   }
